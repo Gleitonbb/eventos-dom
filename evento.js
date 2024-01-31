5eeeeaed3717ae2841bfde21447c1308456ce9ea
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-01-31 11:24:40 -0300

    finalizando treinamento

--- evento.js
+++ evento.js
@@ -1,18 +1,26 @@
-// let time = document.querySelector('#fl') 
-const todos = [...document.querySelectorAll('.time')]
-todos.map((upu, ind)=>{
- upu.addEventListener('click',(evt)=>{
-   const des = evt.target
-   des.classList.add('destaque')
-   console.log(upu. id + 'foi clicado')
-   
-})   
+const caixa1 = document.querySelector('#caixa1')
+const caixa2 = document.querySelector('#caixa2')
+const times = [...document.querySelectorAll('.time')]
+const botao = document.querySelector('#botao')
+
+times.map((upu,por,arr)=>{
+  upu.addEventListener("click",(evt)=>{
+   const todaDiv = evt.target
+   todaDiv.classList.toggle('selecionado')
+  })
 })
-// let msg = () => {
-    
-//  alert('Flamengo maior Time do Brasilo')
-   
-// }
+botao.addEventListener('click',()=>{
+  const todosTimes = [...document.querySelectorAll('.selecionado')]
+  const todosTimes2 = [...document.querySelectorAll('.time:not(.selecionado)')]
+  todosTimes.map((upu,pos)=>{
+   caixa2.appendChild(upu)
+  })
+  todosTimes2.map((upu)=>{
+    caixa1.appendChild(upu)
+  })
+
+})
+
 
 
 
